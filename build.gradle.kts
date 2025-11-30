787ddd8cc17901c615b98698631bce65706d1570
Luis Nafate <luisnafate51@gmail.com>
2025-11-29 18:32:00 -0600

    dependencias gradle

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ktor)
+}
+
+group = "com.example"
+version = "0.0.1"
+
+application {
+    mainClass = "com.example.ApplicationKt"
+}
+
+dependencies {
+    implementation(libs.ktor.server.content.negotiation)
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.serialization.jackson)
+    implementation(libs.jackson.datatype.jsr310)
+    implementation(libs.ktor.server.auth)
+    implementation(libs.ktor.server.auth.jwt)
+    implementation(libs.ktor.server.netty)
+    implementation(libs.logback.classic)
+    
+    // Database
+    implementation(libs.exposed.core)
+    implementation(libs.exposed.dao)
+    implementation(libs.exposed.jdbc)
+    implementation(libs.exposed.java.time)
+    implementation(libs.postgresql)
+    implementation(libs.hikaricp)
+    
+    // Environment variables
+    implementation(libs.dotenv.kotlin)
+    
+    testImplementation(libs.ktor.server.test.host)
+    testImplementation(libs.kotlin.test.junit)
+}
