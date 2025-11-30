2977f7f359df467f93aae1807fbf23f81274e483
Luis Nafate <luisnafate51@gmail.com>
2025-11-30 13:10:00 -0600

    recursos y tests

---
+++ src/test/kotlin/ApplicationTest.kt
@@ -0,0 +1,26 @@
+package com.example
+
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class ApplicationTest {
+
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            configureSerialization()
+            configureRouting()
+        }
+        client.get("/").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            val responseBody = bodyAsText()
+            assertTrue(responseBody.contains("Catálogo Musical") || responseBody.isNotEmpty())
+        }
+    }
+
+}
